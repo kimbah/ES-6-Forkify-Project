cf89ac1ff89c5391d2440005a541b7869dd14bab
kimbah <kim8hughes@iinet.net.au>
2018-11-17 11:02:56 +0800

    Configured bable and polyfills

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,4 +1,4 @@
 // Global app controller
 import num from './test';
-
-console.log(`I imported ${num} from another module`);
+const x = 23;
+console.log(`I imported ${num} from another module called test.js! Varible x is ${x}`);

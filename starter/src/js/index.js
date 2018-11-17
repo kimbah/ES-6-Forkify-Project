5058600f8d0a1fcc504f900770bfad5cf10915c7
kimbah <kim8hughes@iinet.net.au>
2018-11-17 16:13:39 +0800

    Added code to test importing/exporting modules

--- starter/src/js/index.js
+++ starter/src/js/index.js
@@ -1,4 +1,6 @@
 // Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test.js! Varible x is ${x}`);
+import str from './models/Search';
+// import { add as a, multiply as m, ID } from './views/searchView';
+
+import * as searchView from './views/searchView';
+console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${str}`);
